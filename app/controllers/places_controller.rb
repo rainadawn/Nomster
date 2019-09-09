e01ed4a4b73175308b68c1d2d8b251d9a707c74c
Raina Peneton <rainadawn@msn.com>
2019-09-09 23:20:18 +0000

    wip

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,7 @@ class PlacesController < ApplicationController
 	def show
 		@place = Place.find(params[:id])
 		@comment = Comment.new
+		@photo = Photo.new
 	end
 
 	def edit
@@ -59,7 +60,6 @@ class PlacesController < ApplicationController
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
 	end
-
 
 
 end
--- db/migrate/20190908214752_create_comments.rb
+++ db/migrate/20190908214752_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
- t.text :message
+      t.text :message
       t.string :rating
       t.integer :user_id
       t.integer :place_id
